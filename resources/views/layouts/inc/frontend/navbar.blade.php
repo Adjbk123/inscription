ebcaf4f52008427b17aaae82aa51493c93bea82b
Adjbk123 <adjibako123@gmail.com>
2026-02-15 22:28:43 +0100

    reverse

--- resources/views/layouts/inc/frontend/navbar.blade.php
+++ resources/views/layouts/inc/frontend/navbar.blade.php
@@ -6,13 +6,12 @@
 <div class="container-fluid bg-primary text-white d-none d-lg-flex">
     <div class="container py-1 d-flex align-items-center">
         <a href="{{ url('/') }}">
-            <img src="{{  $logo }}" 
-                class="img-fluid" alt="Logo" 
+            <img src="{{  $logo }}" class="img-fluid" alt="Logo"
                 style="height: 70px; width: 70px; border-radius: 50%; object-fit: cover;">
         </a>
         <div class="ms-auto d-flex align-items-center">
             <small class="ms-4">
-                <i class="fa fa-map-marker-alt me-2"></i> 
+                <i class="fa fa-map-marker-alt me-2"></i>
                 {{ $parametres->address ?? 'Adresse par défaut' }}
             </small>
             <small class="ms-4">
@@ -29,29 +28,29 @@
             </small>
             <div class="ms-3 d-flex">
                 @if(!empty($parametres->whatsapp))
-                    <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" 
-                       href="{{ $parametres->whatsapp }}" target="_blank">
+                    <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2"
+                        href="{{ $parametres->whatsapp }}" target="_blank">
                         <i class="fab fa-whatsapp"></i>
                     </a>
                 @endif
 
                 @if(!empty($parametres->facebook))
-                    <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" 
-                       href="{{ $parametres->facebook }}" target="_blank">
+                    <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2"
+                        href="{{ $parametres->facebook }}" target="_blank">
                         <i class="fab fa-facebook-f"></i>
                     </a>
                 @endif
 
                 @if(!empty($parametres->twitter))
-                    <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" 
-                       href="{{ $parametres->twitter }}" target="_blank">
+                    <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2"
+                        href="{{ $parametres->twitter }}" target="_blank">
                         <i class="fab fa-twitter"></i>
                     </a>
                 @endif
 
                 @if(!empty($parametres->youtube))
-                    <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" 
-                       href="{{ $parametres->youtube }}" target="_blank">
+                    <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2"
+                        href="{{ $parametres->youtube }}" target="_blank">
                         <i class="fab fa-youtube"></i>
                     </a>
                 @endif
@@ -72,7 +71,8 @@
             </a>
 
             <!-- Bouton menu mobile -->
-            <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse"
+                data-bs-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -92,44 +92,29 @@
                 <div class="navbar-nav ms-auto align-items-lg-center">
 
                     @guest
-                        {{-- Seuls les visiteurs autorisés: admin et employer --}}
                         @if (Route::has('frontend.login'))
                             <a href="{{ route('frontend.login') }}" class="nav-item nav-link text-success">
                                 <i class="bi bi-box-arrow-in-right"></i> Connectez-vous
                             </a>
                         @endif
-
-                        @if (Route::has('frontend.register'))
-                            <a href="{{ route('frontend.register') }}" class="nav-item nav-link text-success">
-                                <i class="bi bi-person-plus"></i> Inscrivez-vous
-                            </a>
-                        @endif
                     @else
-                        {{-- Vérifie le rôle: admin ou employer --}}
-                        @if(auth()->user()->role === 'administrateur' || auth()->user()->role === 'employer')
-                            <!-- Nom utilisateur -->
-                            <span class="nav-item nav-link fw-bold text-primary d-none d-lg-block">
-                                <i class="fa fa-user"></i> {{ Auth::user()->name }}
-                            </span>
-
-                            <!-- 🔴 Déconnexion grand écran -->
-                            <form method="POST" action="{{ route('logout') }}" class="d-none d-lg-block ms-2">
-                                @csrf
-                                <button type="submit" class="btn btn-danger btn-sm rounded-pill px-3">
-                                    Déconnexion
-                                </button>
-                            </form>
-
-                            <!-- 📱 Déconnexion petit écran -->
-                            <div class="d-lg-none w-100 mt-2">
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle fw-bold text-primary" data-bs-toggle="dropdown">
+                                <i class="fa fa-user me-1"></i> {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end border-0 shadow-sm m-0 rounded-3">
+                                <a href="{{ route('home') }}" class="dropdown-item py-2">
+                                    <i class="fa fa-tachometer-alt me-2 text-primary"></i> Dashboard
+                                </a>
+                                <div class="dropdown-divider"></div>
                                 <form method="POST" action="{{ route('logout') }}">
                                     @csrf
-                                    <button type="submit" class="btn btn-danger w-100 rounded-pill">
-                                        Déconnexion
+                                    <button type="submit" class="dropdown-item py-2 text-danger">
+                                        <i class="fa fa-sign-out-alt me-2"></i> Déconnexion
                                     </button>
                                 </form>
                             </div>
-                        @endif
+                        </div>
                     @endguest
 
                 </div>
